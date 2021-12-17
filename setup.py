7e275095be6222ae6181d671ff1b26aace98cf57
Sugarbell <ccruz4344@gmail.com>
2021-12-16 23:13:54 -0600

    v0.0.1 | Fix Setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='booru',
-    version='0.1.1',
+    version='0.0.1',
     packages=['booru', 'booru.types'],
     url='https://github.com/TaprisSugarbell/Danbooru',
     author='SayuOgiwara',
